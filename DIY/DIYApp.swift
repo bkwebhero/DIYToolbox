1fd38e240d560a052a3f9453c9f81d6c9569c8d6
Benjamin Kelsey <bk@bkwebhero.com>
2021-02-13 16:59:09 -0600

    Initial Commit

---
+++ DIY/DIYApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIYApp.swift
+//  DIY
+//
+//  Created by Benjamin Kelsey on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct DIYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
